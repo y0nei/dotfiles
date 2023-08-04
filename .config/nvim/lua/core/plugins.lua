b6221bfc27837418bab1132f1809c4be098f01bb
y0nei <y0nei@proton.me>
2023-08-04 22:53:42 +0200

    nvim: Lazy-load Comment.nvim on keybinds

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -110,7 +110,14 @@ lazy.setup({
     },
     { -- Better commenting
         "numToStr/Comment.nvim",
-        event = { "BufRead", "BufNewFile" },
+        keys = {
+            { "gcc", mode = "n", desc = "Comment toggle current line" },
+            { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
+            { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
+            { "gbc", mode = "n", desc = "Comment toggle current block" },
+            { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+            { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" }
+        },
         opts = {}
     },
     { -- Auto close bracket pairs
