9c22c7711851910cc1b0eda63f0cdb0e0863adc4
y0nei <y0nei@proton.me>
2024-01-11 21:44:29 +0100

    emacs: Keep UI-customized settings in separate file

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,1 +1,6 @@
 (setq package-enable-at-startup nil)
+
+;; Set the customize file so it doesn't pollute init.el
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
